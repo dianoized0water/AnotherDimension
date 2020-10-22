570803c6150c6ecc227ee0a9943c530e61b0a6be
diana_yang <dyang20@stuy.edu>
2020-10-22 13:18:30 -0400

    first changes

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,10 @@
 public class ArrayOps{
-  
+  public static int sum(int[] arr){
+    int sumArr = -;
+    for (int i=0; i<arr.length; i++) {
+      sumArr += arr[i];
+    }
+    return sumArr;
+  }
+
 }
--- Tester.java
+++ Tester.java
@@ -1,3 +1,5 @@
 public class Tester{
-  
+  public static void main(String args[]){
+
+  }
 }
