8f5808ce146032e6d48cfbb4767403e0f0a0a0d6
dianoized0water <62660947+dianoized0water@users.noreply.github.com>
2020-10-22 22:30:55 -0400

    Update ArrayOps.java

--- ArrayOps.java
+++ ArrayOps.java
@@ -36,4 +36,14 @@ public class ArrayOps{
   public static int sum(int[][] arr){
     return ArrayOps.sum(ArrayOps.sumRows(arr));
   }
+  
+  public static int[] sumCols(int[][] matrix){
+    int[] colsArray = new int[matrix[0].length];
+    for (int i=0; i<matrix[0].length; i++){
+      for (int j=0; j<matrix.length; j++){
+        colsArray[i] += matrix[j][i];
+      }
+    }
+    return colsArray;
+  }
 }
