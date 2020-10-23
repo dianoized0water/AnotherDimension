20015bf2d07362e23a5b80b92501ee71e0a4b59e
dianoized0water <62660947+dianoized0water@users.noreply.github.com>
2020-10-22 23:03:44 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -2,16 +2,32 @@ import java.util.Arrays;
 
 public class Tester{
   public static void main(String args[]){
-    /* int[] B = {  1, 3, 5 };
+   /*
+    int[] B = {  1, 3, 5 };
     System.out.println(ArrayOps.sum(B));
-    System.out.println(ArrayOps.largest(B)); */
+    System.out.println(ArrayOps.largest(B));
 
-    /* int[][]  A  =  {  {  1,  0, 12, -1 },
-                  {  7, -2,  2,  1 },
-                  { -5, -2,  2, -9 }
-               };
+   int[][]  A  =  {  {  1,  0, 12, -1 },
+                     {  7, -2,  2,  1 },
+                     { -5, -2,  2, -9 }};
     System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
     System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
-    System.out.println(ArrayOps.sum(A)); */
+    System.out.println(ArrayOps.sum(A));
+    System.out.println(Arrays.toString(ArrayOps.sumCols(A)));
+
+    int[][] C = { {  1,  2, 3, 4 },
+                  {  2, 3,  4,  1 },
+                  { 3, 4,  1, 2 } };
+    int[][] D = { {  1,  1, 1 },
+                  {  2, 2, 2 },
+                  { 3,  3, 3 } };
+    int[][] E = { { 2, 2, 2},
+                  { 2, 2, 2}};
+    int[][] F = { { 2, 4, 4},
+                  { 2, 2, 2}};
+    System.out.println(ArrayOps.isRowMagic(E));
+    System.out.println(ArrayOps.isColMagic(E));
+    System.out.println(ArrayOps.isLocationMagic(F, 1, 1));
+    */
   }
 }
